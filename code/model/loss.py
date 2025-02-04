04ee7a05b99b7c59a89f6c5ce9abbfc1bc6847cc
He Changpeng <hcp6897@gsupl.edu.cn>
2025-02-04 11:45:48 +0800

    modified

--- code/model/loss.py
+++ code/model/loss.py
@@ -37,20 +37,19 @@ class SALLoss(GenLoss):
 
 
 class SALReconLoss(GenLoss):
-    def __init__(self, manifold_pnts_weight,unsigned):
+    def __init__(self, manifold_pnts_weight, unsigned):
         super().__init__(manifold_pnts_weight)
         self.unsigned = unsigned
 
-    def forward(self, manifold_pnts_pred,
+    def forward(self, 
+                manifold_pnts_pred,
                 nonmanifold_pnts_pred,
                 nonmanifold_gt,
                 delta = None,
                 weight=None,
                 latent_reg=None):
-
-
-
-        recon_term = torch.abs(nonmanifold_pnts_pred.squeeze().abs() - 1).mean() + self.manifold_pnts_weight*manifold_pnts_pred.abs().mean()
+        recon_term = torch.abs(nonmanifold_pnts_pred.squeeze().abs() - 1).mean() + \
+            self.manifold_pnts_weight*manifold_pnts_pred.abs().mean()
 
         loss = recon_term
 
