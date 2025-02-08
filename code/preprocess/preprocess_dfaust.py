452e844bdc185976bbf4070f4dca9d10df72ac39
hcp <hcp6897@gsupl.edu.cn>
2025-02-08 14:56:04 +0800

    explain about the function

--- code/preprocess/preprocess_dfaust.py
+++ code/preprocess/preprocess_dfaust.py
@@ -9,8 +9,8 @@ import argparse
 import trimesh
 from trimesh.sample import sample_surface
 import numpy as np
-
 from scipy.spatial import cKDTree
+
 from CGAL.CGAL_Kernel import Point_3
 from CGAL.CGAL_Kernel import Triangle_3
 from CGAL.CGAL_AABB_tree import AABB_tree_Triangle_3_soup
@@ -71,7 +71,10 @@ if __name__ == '__main__':
                         print ('loading : {0}'.format(os.path.join(source, shape)))
                         mesh = trimesh.load(os.path.join(source, shape) + '.ply')
                         
-                        # 采样点云
+                        """采样点云
+                        这段代码从网格中采样250000个点，并将这些点中心化（即减去中心点坐标），最后进行缩放（虽然缩放因子为1，实际没有缩放效果）。
+                        这种操作通常用于点云数据的预处理，使得点云数据在空间中居中，便于后续的处理或分析。
+                        """
                         sample = sample_surface(mesh, 250000)   # 250000 points
                         center = np.mean(sample[0], axis=0)
                         pnts = sample[0]
@@ -79,34 +82,43 @@ if __name__ == '__main__':
                         scale = 1
                         pnts = pnts/scale
                         
-                        # 采样三角面片
+                        """采样三角面片
+                        对网格中的每个三角面片进行归一化处理，使其相对于网格中心点进行平移，并通过缩放因子 scale 进行缩放.
+                        处理后的三角面片存储在 triangles 列表中，以便后续使用.
+                        这种处理通常用于将网格数据标准化，以便在不同的算法或模型中使用.
+                        """
                         triangles = []  # triangle soups
                         for tri in mesh.triangles:
-                            a = Point_3((tri[0][0] - center[0])/scale, (tri[0][1]- center[1])/scale, (tri[0][2]- center[2])/scale)
-                            b = Point_3((tri[1][0]- center[0])/scale, (tri[1][1]- center[1])/scale, (tri[1][2]- center[2])/scale)
-                            c = Point_3((tri[2][0]- center[0])/scale, (tri[2][1]- center[1])/scale, (tri[2][2]- center[2])/scale)
-                            triangles.append(Triangle_3(a, b, c))
+                            a = Point_3((tri[0][0] - center[0])/scale, (tri[0][1] - center[1])/scale, (tri[0][2] - center[2])/scale)   # 三角面片的第一个顶点
+                            b = Point_3((tri[1][0] - center[0])/scale, (tri[1][1] - center[1])/scale, (tri[1][2] - center[2])/scale)   # 三角面片的第二个顶点
+                            c = Point_3((tri[2][0] - center[0])/scale, (tri[2][1] - center[1])/scale, (tri[2][2] - center[2])/scale)   # 三角面片的第三个顶点
+                            triangles.append(Triangle_3(a, b, c)) # 将处理后的三个顶点 a, b, c 组成一个新的三角面片 Triangle_3，并将其添加到 triangles 列表中.
                         
-                        tree = AABB_tree_Triangle_3_soup(triangles) # AABB tree for triangle soups
+                        # AABB tree for triangle soups
+                        tree = AABB_tree_Triangle_3_soup(triangles) 
 
-                        # 为点云中的每个点定义两个不同的噪声标准差 sigmas 和 sigmas_big, 可以理解为该点的局部密度或噪声水平.
+                        """
+                        每个点定义两个不同的噪声标准差：局部噪声标准差 sigmas (可以理解为该点的局部密度或噪声水平) 和 全局噪声标准差 sigmas_big.
+                        """
                         sigmas = []
-                        ptree = cKDTree(pnts) # KDTree for points
+                        ptree = cKDTree(pnts) # 构建点云的KDtree
                         i = 0
-                        # 计算点云中每个点的第51个最近邻点的距离
-                        for p in np.array_split(pnts, 100, axis=0): # split pnts into 100 parts
-                            d = ptree.query(p, 51) # 返回两个数组：第一个数组是距离，第二个数组是索引. 
-                            sigmas.append(d[0][:, -1]) # 对于每个点，提取第 51 个最近邻点的距离，并将其添加到 sigmas 列表中. 
+                        # 计算每个点的51个最近邻点(K-nearest neighbors)的距离
+                        for p in np.array_split(pnts, 100, axis=0):     # 将点云分成100个部分进行处理
+                            d = ptree.query(p, 51)  # 计算查询点的KNN（K=51）距离值，返回两个数组：第一个数组是距离，第二个数组是索引. 
+                            sigmas.append(d[0][:, -1])  # 对于每个点，提取第51个最近邻距离，来估计局部密度或噪声水平. 
 
                             i = i+1
 
+                        # 局部噪声标准差 sigmas
                         sigmas = np.concatenate(sigmas)
+                        # 全局噪声标准差 sigmas_big，对所有点都是相同的.
                         sigmas_big = opt.sigma * np.ones_like(sigmas)
 
                         # 保存点云
                         np.save(output_file + '.npy', pnts)
 
-                        # 通过添加随机噪声来生成新的样本点，使用两个不同的噪声标准差（sigmas 和 sigmas_big）来对原始点集 pnts 进行扰动，然后将扰动后的点集拼接在一起.
+                        # 使用两个不同的噪声标准差（sigmas 和 sigmas_big）来对原始点集 pnts 进行扰动，然后将扰动后的点集拼接在一起.
                         sample = np.concatenate(
                             [pnts + np.expand_dims(sigmas, -1) * np.random.normal(0.0, 1.0, size=pnts.shape), 
                              pnts + np.expand_dims(sigmas_big, -1) * np.random.normal(0.0, 1.0, size=pnts.shape)], 
